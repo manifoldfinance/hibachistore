d921c2d928569a4bc311c0f16f42f963470ca190
sam <sam@freighttrust.com>
2021-01-20 02:37:19 -0800

    feat(provider): alchemyapi provider and minor tweaks

---
+++ index.ts
@@ -0,0 +1,9 @@
+mport { Wallet, Contract, providers } from "ethers";
+import { Web3Provider } from "ethers/providers";
+import { deployContract } from "ethereum-waffle";
+
+import { expandTo18Decimals } from "./utilities";
+
+export async function initialiseDemo() {}
+
+initialiseDemo();
