57993ef1ec6f9346f22ad42b0960b8ab147a88fd
sam bacha <sam@freighttrust.com>
2021-01-21 16:44:51 -0800

    refactor(dependencies): npm tooling and pragma (#4)

--- index.js
+++ index.js
@@ -1,157 +1,152 @@
+const SushiRollArtifacts = require("../../build/contracts/HibachiStore.json");
 
-const SushiRollArtifacts = require('../../build/contracts/HibachiStore.json')
-
-const Web3 = require('web3')
-const BN = Web3.utils.BN
-const ZeroClientProvider = require('web3-provider-engine/zero.js')
+const Web3 = require("web3");
+const BN = Web3.utils.BN;
+const ZeroClientProvider = require("web3-provider-engine/zero.js");
 
 class SushiRoll {
-  constructor (options) {
+  constructor(options) {
+    this.SushiRoll = null;
 
-    this.SushiRoll = null
-
-    this.pollingInterval = null
-    this.account = null
-    this.unlocked = false
-    this.balanceWei = 0
-    this.balance = 0
-    this.address = 'REPLACE_WITH_CONTRACT_ADDRESS'
-    this.genesisBlock = 0
-    this.loading = false
+    this.pollingInterval = null;
+    this.account = null;
+    this.unlocked = false;
+    this.balanceWei = 0;
+    this.balance = 0;
+    this.address = "REPLACE_WITH_CONTRACT_ADDRESS";
+    this.genesisBlock = 0;
+    this.loading = false;
     this.options = {
-      readonlyRpcURL: 'https://mainnet.infura.io',
+      readonlyRpcURL: "https://mainnet.infura.io",
       autoInit: true,
       getPastEvents: false,
       watchFutureEvents: false,
-      connectionRetries: 3
-    }
-    Object.assign(this.options, options)
-    if (this.options.autoInit) this.initWeb3()
+      connectionRetries: 3,
+    };
+    Object.assign(this.options, options);
+    if (this.options.autoInit) this.initWeb3();
   }
 
-  // 
-  ContractShark () {
-    console.log('ContractShark Initalized...')
+  //
+  ContractShark() {
+    console.log("ContractShark Initalized...");
   }
 
   /*
    * Connect
    */
 
-  initWeb3 () {
+  initWeb3() {
     return new Promise((resolve, reject) => {
-
-      let web3Provider = false
+      let web3Provider = false;
 
       // check for metamask
       if (global.web3) {
-        web3Provider = global.web3.currentProvider
+        web3Provider = global.web3.currentProvider;
         // attempt to try again if no web3Provider
-      } else if (this.options.connectionRetries > 0){
-        this.options.connectionRetries -= 1
+      } else if (this.options.connectionRetries > 0) {
+        this.options.connectionRetries -= 1;
         setTimeout(() => {
-          this.initWeb3().then(resolve).catch((error) => {
-            reject(new Error(error))
-          })
-        }, 1000)
+          this.initWeb3()
+            .then(resolve)
+            .catch(error => {
+              reject(new Error(error));
+            });
+        }, 1000);
         // revert to a read only version using infura endpoint
       } else {
-        this.readOnly = true
+        this.readOnly = true;
         web3Provider = ZeroClientProvider({
-          getAccounts: function(){},
-          rpcUrl: this.options.readonlyRpcURL
-        })
+          getAccounts: function () {},
+          rpcUrl: this.options.readonlyRpcURL,
+        });
       }
 
       if (web3Provider) {
-        global.web3 = new Web3(web3Provider)
-        this.startChecking()
+        global.web3 = new Web3(web3Provider);
+        this.startChecking();
 
-        if (this.options.getPastEvents) this.getPastEvents()
-        if (this.options.watchFutureEvents) this.watchFutureEvents()
+        if (this.options.getPastEvents) this.getPastEvents();
+        if (this.options.watchFutureEvents) this.watchFutureEvents();
       }
-    })
-
+    });
   }
 
   /*
    * Check every second for switching network or switching wallet
    */
 
-  startChecking () {
-    if (this.pollingInterval) clearInterval(this.pollingInterval)
+  startChecking() {
+    if (this.pollingInterval) clearInterval(this.pollingInterval);
     this.getGenesisBlock()
-    .then(() => {
-      this.pollingInterval = setInterval(this.check.bind(this), 1000)
-    })
-    .catch((err) => {
-      throw new Error(err)
-    })
+      .then(() => {
+        this.pollingInterval = setInterval(this.check.bind(this), 1000);
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
   }
 
-  check () {
+  check() {
     this.checkNetwork()
-    .then(this.checkAccount.bind(this))
-    .catch((error) => {
-      console.error(error)
-      throw new Error(error)
-    })
+      .then(this.checkAccount.bind(this))
+      .catch(error => {
+        console.error(error);
+        throw new Error(error);
+      });
   }
 
-  checkNetwork () {
+  checkNetwork() {
     return global.web3.eth.net.getId((err, netId) => {
-      if (err) console.error(err)
+      if (err) console.error(err);
       if (!err && this.network !== netId) {
-        this.network = netId
-        return this.deployContract()
+        this.network = netId;
+        return this.deployContract();
       }
-    })
+    });
   }
 
-  deployContract () {
-    if (!this.address || this.address === 'REPLACE_WITH_CONTRACT_ADDRESS') return new Error('Please provide a contract address')
-    this.SushiRoll = new global.web3.eth.Contract(SushiRollArtifacts.abi, this.address)
+  deployContract() {
+    if (!this.address || this.address === "REPLACE_WITH_CONTRACT_ADDRESS")
+      return new Error("Please provide a contract address");
+    this.SushiRoll = new global.web3.eth.Contract(SushiRollArtifacts.abi, this.address);
   }
 
-  checkAccount () {
+  checkAccount() {
     return global.web3.eth.getAccounts((error, accounts) => {
-      if (error) throw new Error(error)
+      if (error) throw new Error(error);
       if (accounts.length && this.account !== accounts[0]) {
-        this.unlocked = true
-        this.account = accounts[0]
+        this.unlocked = true;
+        this.account = accounts[0];
       } else if (!accounts.length) {
-        this.unlocked = false
-        this.account = null
+        this.unlocked = false;
+        this.account = null;
       }
-    })
+    });
   }
-
 
   /*
-   * Not Yet Implemented 
-   * 
+   * Not Yet Implemented
+   *
    */
 
-  getGenesisBlock () {
+  getGenesisBlock() {
     return new Promise((resolve, reject) => {
-      resolve()
-    })
+      resolve();
+    });
   }
 
-  getPastEvents () {
+  getPastEvents() {
     return new Promise((resolve, reject) => {
-      resolve()
-    })
+      resolve();
+    });
   }
 
-  watchFutureEvents () {
+  watchFutureEvents() {
     return new Promise((resolve, reject) => {
-      resolve()
-    })
+      resolve();
+    });
   }
-
-
-
 
   /*
    *
@@ -159,21 +154,27 @@ class SushiRoll {
    *
    */
 
-  oldRouter () {
-    return this.SushiRoll.methods.oldRouter().call()
-    .then((resp) => {
-      return resp
-    }).catch((err) => {
-      console.error(err)
-    })
+  oldRouter() {
+    return this.SushiRoll.methods
+      .oldRouter()
+      .call()
+      .then(resp => {
+        return resp;
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
-  router () {
-    return this.SushiRoll.methods.router().call()
-    .then((resp) => {
-      return resp
-    }).catch((err) => {
-      console.error(err)
-    })
+  router() {
+    return this.SushiRoll.methods
+      .router()
+      .call()
+      .then(resp => {
+        return resp;
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 
   /*
@@ -182,35 +183,58 @@ class SushiRoll {
    *
    */
 
-  migrateWithPermit (tokenA, tokenB, liquidity, amountAMin, amountBMin, deadline, v, r, s) {
-    if (!this.account) return Promise.reject(new Error('Unlock Account'))
-    return this.SushiRoll.methods.migrateWithPermit(tokenA, tokenB, new BN(liquidity, 10), new BN(amountAMin, 10), new BN(amountBMin, 10), new BN(deadline, 10), new BN(v, 10), r, s).send({from: this.account})
-    .on('transactionHash', (hash) => {
-      console.log(hash)
-      this.loading = true
-    })
-    .then((resp) => {
-      this.loading = false
-      return resp
-    }).catch((err) => {
-      this.loading = false
-      console.error(err)
-    })
+  migrateWithPermit(tokenA, tokenB, liquidity, amountAMin, amountBMin, deadline, v, r, s) {
+    if (!this.account) return Promise.reject(new Error("Unlock Account"));
+    return this.SushiRoll.methods
+      .migrateWithPermit(
+        tokenA,
+        tokenB,
+        new BN(liquidity, 10),
+        new BN(amountAMin, 10),
+        new BN(amountBMin, 10),
+        new BN(deadline, 10),
+        new BN(v, 10),
+        r,
+        s,
+      )
+      .send({ from: this.account })
+      .on("transactionHash", hash => {
+        console.log(hash);
+        this.loading = true;
+      })
+      .then(resp => {
+        this.loading = false;
+        return resp;
+      })
+      .catch(err => {
+        this.loading = false;
+        console.error(err);
+      });
   }
-  migrate (tokenA, tokenB, liquidity, amountAMin, amountBMin, deadline) {
-    if (!this.account) return Promise.reject(new Error('Unlock Account'))
-    return this.SushiRoll.methods.migrate(tokenA, tokenB, new BN(liquidity, 10), new BN(amountAMin, 10), new BN(amountBMin, 10), new BN(deadline, 10)).send({from: this.account})
-    .on('transactionHash', (hash) => {
-      console.log(hash)
-      this.loading = true
-    })
-    .then((resp) => {
-      this.loading = false
-      return resp
-    }).catch((err) => {
-      this.loading = false
-      console.error(err)
-    })
+  migrate(tokenA, tokenB, liquidity, amountAMin, amountBMin, deadline) {
+    if (!this.account) return Promise.reject(new Error("Unlock Account"));
+    return this.SushiRoll.methods
+      .migrate(
+        tokenA,
+        tokenB,
+        new BN(liquidity, 10),
+        new BN(amountAMin, 10),
+        new BN(amountBMin, 10),
+        new BN(deadline, 10),
+      )
+      .send({ from: this.account })
+      .on("transactionHash", hash => {
+        console.log(hash);
+        this.loading = true;
+      })
+      .then(resp => {
+        this.loading = false;
+        return resp;
+      })
+      .catch(err => {
+        this.loading = false;
+        console.error(err);
+      });
   }
 
   /*
@@ -218,10 +242,6 @@ class SushiRoll {
    * Events
    *
    */
-
-
-
-
 }
 
-module.exports = SushiRoll
+module.exports = SushiRoll;
