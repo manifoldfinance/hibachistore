b2a006c3005f876edcdeb210d465876812513ce4
sam <sam@freighttrust.com>
2021-01-23 06:29:22 -0800

    rebase continue

--- test/index.ts
+++
@@ -1,9 +0,0 @@
-import { Wallet, Contract, providers } from "ethers";
-import { Web3Provider } from "ethers/providers";
-import { deployContract } from "ethereum-waffle";
-
-import { expandTo18Decimals } from "./utilities";
-
-export async function initialiseDemo() {}
-
-initialiseDemo();
