c18fe57a24231850c9b21e2d0711c9b8d66c5065
sam <sam@freighttrust.com>
2021-01-23 05:25:33 -0800

    ci(msolid): setup tessting env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -72,7 +72,7 @@ const config: HardhatUserConfig = {
     settings: {
       // https://hardhat.org/hardhat-network/#solidity-optimizer-support
       optimizer: {
-        enabled: true,
+        enabled: false,
         runs: 200,
       },
     },
---
+++ msolid/README.md
@@ -0,0 +1,6 @@
+### Test files module 
+
+### These Private Keys Are for Testing purposes only!
+
+
+
